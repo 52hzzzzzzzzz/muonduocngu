9bf46577c888f0993b4e495e3c1ff7ec6cc22428
Bành <113755734+52hzzzzzzzzz@users.noreply.github.com>
2023-10-28 02:08:24 +0700

    Delete rowder directory

--- rowder/python/lab2/HinhHoc/hinh_hoc.py
+++
@@ -1,8 +0,0 @@
-class HinhHoc:
-    def __init__(self, cd:float):
-        self.canh = cd
-    def TinhDienTich(self)->float:
-        pass
-    def __str__(self) -> str:
-        return 'Hinh '
-    
